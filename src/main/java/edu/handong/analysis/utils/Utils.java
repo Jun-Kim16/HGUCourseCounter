8c2625233fae7f965665471efc99735a6c8aaf54
Jun-Kim16 <21600146@handong.edu>
2019-05-26 20:54:19 +0900

    HW5

--- src/main/java/edu/handong/analysis/utils/Utils.java
+++ src/main/java/edu/handong/analysis/utils/Utils.java
@@ -2,28 +2,36 @@ package edu.handong.analysis.utils;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Utils {
 	public static ArrayList<String> getLines(String file, boolean removeHeader) {
 		ArrayList<String> lines = new ArrayList();
-		
-		
+
 		BufferedReader bf = null;
 		try {
 			bf = new BufferedReader(new FileReader(file));
 			if (removeHeader) {
 				bf.readLine();
-				while (bf.readLine()!=null)
-					lines.add(bf.readLine());
-				return lines;
+				while (true) {
+					String a = bf.readLine();
+					if(a!=null) lines.add(a);
+					else break;
+					
+				}
 			} else {
-				while (bf.readLine()!=null)
-					lines.add(bf.readLine());
+				while (true) {
+					String a = bf.readLine();
+					if(a!=null) lines.add(a);
+					else break;
+					
+				}
 			}
 			bf.close();
 		} catch (FileNotFoundException e) {
@@ -35,16 +43,17 @@ public class Utils {
 	}
 
 	public static void writeAFile(ArrayList<String> lines, String targetFileName) {
-	
+
 		BufferedWriter bf = null;
 		try {
 			bf = new BufferedWriter(new FileWriter(targetFileName, false));
 			bf.write("StudentID, TotalNumberOfSemestersRegistered, Semester, NumCoursesTakenInTheSemester");
 			bf.newLine();
-			for(String l : lines) {
-				 bf.write(l);
-				 bf.newLine();
+			for (String l : lines) {
+				bf.write(l);
+				bf.newLine();
 			}
+			bf.flush();
 			bf.close();
 		} catch (IOException e) {
 			e.printStackTrace();
